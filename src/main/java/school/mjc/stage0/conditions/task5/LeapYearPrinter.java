dfbc556b09ccb6333ef4e3d80172275e470014b6
tabletka97 <mtabletka.by@gmail.com>
2023-02-01 15:16:09 +0300

    task5 done

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        year = year == 1900 ? ++year : year;
+        switch (year % 4) {
+            case 0:
+                System.out.println("leap");
+                break;
+            default:
+                System.out.println("not leap");
+        }
     }
 }
